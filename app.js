e5cbfb182e2ac20c5c2b592bebcff5a301044d32
Svaazz <Svaazz@users.noreply.github.com>
2024-10-04 05:34:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const ctx = canvas.getContext('2d');
 let logData = null;
 let qslImage = new Image();
 
-// Función para cargar el archivo log.adi
+
 fetch('log.adi')
   .then(response => response.text())
   .then(data => {
-    logData = data.replace(/\r?\n|\r/g, ' ');  // Eliminar saltos de línea para facilitar la búsqueda
+    logData = data.replace(/\r?\n|\r/g, ' ');  // Eliminar saltos de línea 
   })
   .catch(error => console.error('Error al cargar el archivo ADIF:', error));
 
-// Función para cargar la imagen QSL.png
+
 qslImage.src = 'QSL.png';
 
-// Función para buscar el distintivo en el log
+// Función para buscar distintivo en el log
 function searchForText(searchText) {
     // Convert both the bigText and searchText to uppercase for case-insensitive search
     const bigTextUpper = logData.toUpperCase();
@@ -46,11 +46,11 @@ function generateCard() {
     alert('El distintivo no está en el log. Por favor, contacta con info@example.com para más información.');
     return;
   } else {
-    // Redibujar la imagen QSL
+   
     ctx.clearRect(0, 0, canvas.width, canvas.height);  // Limpiar el canvas
     ctx.drawImage(qslImage, 0, 0, canvas.width, canvas.height);
   
-    // Dibujar el distintivo centrado en la postal
+    // Dibujar el distintivo centrado en la qsl
     ctx.font = "80px Arial";
     ctx.fillStyle = "orange";
     ctx.textAlign = "center";
@@ -58,11 +58,18 @@ function generateCard() {
     const x = canvas.width / 2;
     const y = canvas.height / 2;
     ctx.fillText(callsign, x, y);
+    // Dibujar los detalles del QSO (hora, banda, y fecha) debajo del distintivo
+    cert = `Certificamos que `;
+    ific = `realizó el correspondiente QSO con AO180GC (ejemplo).`
+    ctx.font = "30px Arial";
+    ctx.fillText(cert, x, y + 60);
+    ctx.fillText(ific, x, y - 60);
     ctx.drawImage(qslImage, 0, 0, canvas.width, canvas.height);
+    
   }
 }
 
-// Función para descargar la postal
+// descargar la qsl
 function downloadCard() {
   const link = document.createElement('a');
   link.download = 'QSL_180GC.png';
